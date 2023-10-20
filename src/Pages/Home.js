7da9f2b28aa2103b0a754d1d1af501658b70dc4c
Abhinav Kumar <ady.abhinav.ak@gmail.com>
2023-10-20 16:31:18 +0100

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -47,7 +47,7 @@ data-aos="fade-down" data-aos-once="true"
               color: 'white',
             }}
           >
-            <h1 style={{ zIndex: '10' }}>Welcome to Anime</h1>
+            <h1 style={{ zIndex: '10' }}>Welcome to Aniplix</h1>
           </div>
           <form className='search-form' onSubmit={handleSubmit}>
             <input className='search-input' type='text' placeholder='Search...' value={searchQuery} onChange={handleChange}/>
@@ -65,16 +65,16 @@ data-aos="fade-down" data-aos-once="true"
 
       <div className="paragraph" style={{ margin: '20%' }}>
         <p style={{color: 'white' }}>
-          Anime is a free website for watching anime online that offers subbed or dubbed anime in ultra HD quality without any registration or payment. With over 1 billion monthly searches for anime related topics, it's no surprise that there has been a sharp rise in the number of free anime streaming sites. However, not all sites are created equal, which is why anime was created as one of the best free anime streaming sites for all anime fans worldwide.
+          Aniplix is a free website for watching aniplix online that offers subbed or dubbed aniplix in ultra HD quality without any registration or payment. With over 1 billion monthly searches for aniplix related topics, it's no surprise that there has been a sharp rise in the number of free aniplix streaming sites. However, not all sites are created equal, which is why aniplix was created as one of the best free aniplix streaming sites for all aniplix fans worldwide.
         </p>
         <p style={{ color: 'white' }}>
-          Anime is a safe site to watch anime, with only one ad to cover server costs and constant scanning to ensure that all ads are clean. If you find any suspicious ads, you can forward the information to Anime, and they will remove them.
+          Aniplix is a safe site to watch aniplix, with only one ad to cover server costs and constant scanning to ensure that all ads are clean. If you find any suspicious ads, you can forward the information to Aniplix, and they will remove them.
         </p>
         <p style={{ color: 'white' }}>
-          Anime stands out from other anime streaming sites because of its focus on safety, content library, quality/resolution, streaming experience, updates, user interface, device compatibility, and customer care. The site offers popular, classic, and current anime titles from all genres, including action, drama, kids, fantasy, horror, mystery, police, romance, school, comedy, music, game, and more. All titles come with English subtitles or are dubbed in multiple languages, and users can choose from various quality settings to ensure a smooth streaming experience.
+          Aniplix stands out from other aniplix streaming sites because of its focus on safety, content library, quality/resolution, streaming experience, updates, user interface, device compatibility, and customer care. The site offers popular, classic, and current Aniplix titles from all genres, including action, drama, kids, fantasy, horror, mystery, police, romance, school, comedy, music, game, and more. All titles come with English subtitles or are dubbed in multiple languages, and users can choose from various quality settings to ensure a smooth streaming experience.
         </p>
         <p style={{ color: 'white' }}>
-          If you have trouble accessing Anime, you can try anix or gogoanime, Anime is committed to providing a trustworthy and safe site for anime streaming and offers excellent customer service. Give Anime a try and spread the word if you like it.
+          If you have trouble accessing Aniplix, you can try anix or gogoanime, Aniplix is committed to providing a trustworthy and safe site for Aniplix streaming and offers excellent customer service. Give Aniplix a try and spread the word if you like it.
         </p>
       </div>
     </>
