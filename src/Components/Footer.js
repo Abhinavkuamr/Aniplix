1d8f6995255e533f746d713d22300c0fc35a1521
Abhinav Kumar <ady.abhinav.ak@gmail.com>
2023-10-20 16:31:39 +0100

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,7 +6,7 @@ export default class Footer extends Component {
     return (
       <>
       <div class="copyright container">
-    <a href="#" class="logo"><i class="fas fa-infinity"></i>Anime.</a>
+    <a href="#" class="logo"><i class="fas fa-infinity"></i>Aniplix.</a>
     <p>&#169; It's your Anime fan Boy. All rights reserved.</p>
     </div>
       </>
